ce49e523f4067916f15655d57c569b9361047624
Ayushman <avenuedesign768@gmail.com>
2026-07-31 21:53:13 +0530

    JS code

---
+++ app.js
@@ -0,0 +1,49 @@
+// alert("Welcome to TCM")
+// console.log(574587548 * 4394394839)
+// document.write("Hello i am from TCM")
+// let deleteData = confirm("Are You want to delete this ?");
+// console.log(deleteData)
+
+// let qty = prompt("Enter your QTY...");
+// document.write("Your Item QTY is: ", qty);
+
+// console.error("Limit Exeed")
+
+// var , let, const
+
+// Declearation
+// var name;
+
+// // Assign
+// name = "Ansh";
+// // Assign
+// name = "Ayushman";
+// // Assign
+// name = "Rajesh";
+
+// // Declearation
+// var name;
+// name = "Amrit";
+
+// console.log(name)
+
+
+// Declearation
+// let name;
+
+// // Assign
+// name = "Rajesh";
+// // Assign
+// name = "Raju";
+
+// // Declearation
+// let name;
+
+// // Assign
+// name = "Ramesh";
+
+// console.log(name)
+
+// const student_id = 599;
+// console.log(student_id)
+
