245e6b4ddcde273cfcd4b3c84a2dda50329d7dea
Ayushman <avenuedesign768@gmail.com>
2026-08-06 21:56:58 +0530

    JS lOOP AND IF ELSE

--- app.js
+++ app.js
@@ -14,14 +14,14 @@
 // Declearation
 // var name;
 
-// // Assign
+// // // Assign
 // name = "Ansh";
-// // Assign
+// // // Assign
 // name = "Ayushman";
-// // Assign
+// // // Assign
 // name = "Rajesh";
 
-// // Declearation
+// // // Declearation
 // var name;
 // name = "Amrit";
 
@@ -31,19 +31,230 @@
 // Declearation
 // let name;
 
-// // Assign
+// // // Assign
 // name = "Rajesh";
-// // Assign
+// // // Assign
 // name = "Raju";
 
-// // Declearation
-// let name;
+// // // Declearation
+// // let name;
 
-// // Assign
+// // // Assign
 // name = "Ramesh";
 
 // console.log(name)
 
-// const student_id = 599;
-// console.log(student_id)
+// // const student_id = 599;
+// // console.log(student_id)
 
+// // Premitive 
+// // 1. string = "Ayushman"; 
+// let name = "45";
+// console.log(typeof(name))
+// 2. Number = 467
+// let number = 48938938;
+// console.log(typeof(number))
+// 3. Boolean = true 
+// let elegible = true;
+// console.log(typeof(elegible))
+// 4. Undefiend
+// let name;
+// console.log(typeof(name))
+// 5. null = 
+// let salary = null;
+// console.log(typeof(salary))
+// let bigint = BigInt(2308498398439839)
+// console.log(typeof(bigint))
+
+
+
+// non premotive
+
+// Array 
+// let users = ["Ayushman", "Harsh", "Rakesh", "Akram"];
+// console.log(users[3])
+
+let fruits = ["Apple", "Banana", "Grapes"]
+// console.log(typeof (fruits))
+
+// let car = color, price, ratings, type, showroom
+// let user = name, surname, city, age, phone, email
+
+// let allUsers = [
+//   {
+//     name: "Ayushman",
+//     age: 23,
+//     city: "waraseoni"
+//   },
+//   {
+//     name: "Akram",
+//     age: 22,
+//     city: "Katani"
+//   },
+//   {
+//     name: "Shivam",
+//     age: 24,
+//     city: "Balaghat"
+//   }
+// ]
+// console.log(allUsers[2].name);
+
+let emp  = [
+    {
+        name: "Ayushman",
+        age: 23,
+        city: "waraseoni"
+    },
+      {
+        name: "Akram",
+        age: 22,
+        city: "Katani"
+    },
+      {
+        name: "Shivam",
+        age: 24,
+        city: "Balaghat"
+    },
+      {
+        name: "Ayushman",
+        age: 23,
+        city: "waraseoni"
+    },
+      {
+        name: "Akram",
+        age: 22,
+        city: "Katani"
+    },
+      {
+        name: "Shivam",
+        age: 24,
+        city: "Balaghat"
+    }
+    ,  {
+        name: "Ayushman",
+        age: 23,
+        city: "waraseoni"
+    },
+      {
+        name: "Akram",
+        age: 22,
+        city: "Katani"
+    },
+      {
+        name: "Shivam",
+        age: 24,
+        city: "Balaghat"
+    },  {
+        name: "Ayushman",
+        age: 23,
+        city: "waraseoni"
+    },
+      {
+        name: "Akram",
+        age: 22,
+        city: "Katani"
+    },
+      {
+        name: "Shivam",
+        age: 24,
+        city: "Balaghat"
+    },  {
+        name: "Ayushman",
+        age: 23,
+        city: "waraseoni"
+    },
+      {
+        name: "Akram",
+        age: 22,
+        city: "Katani"
+    },
+      {
+        name: "Shivam",
+        age: 24,
+        city: "Balaghat"
+    },
+     {
+        name: "Rajesh",
+        age: 24,
+        city: "Balaghat"
+    },
+     {
+        name: "Mohan",
+        age: 24,
+        city: "Balaghat"
+    }
+]
+
+// for loop
+
+// for(let agent = 0; agent < emp.length; agent++){
+//   console.log(agent, typeof(agent))
+//   // console.log(emp[agent].name)
+// }
+
+// for in loop
+// for(let agent in emp){
+//   console.log(emp[agent].name)
+// }
+
+// for of loop
+// for(let agent of emp){
+//   console.log(agent)
+// }
+
+// for each loop
+// emp.forEach(agent => {console.log(agent)})
+
+// while loop
+// let x = 50;
+
+// while(x <= 10){
+//   console.log(x);
+//   x++;
+// }
+
+// do while loop
+// let x = 50;
+
+// do{
+//   console.log(x);
+//   x++;
+// }
+// while(x <= 10);
+
+
+// console.log(emp)
+
+// console.log("Hello I am ", emp[0].name, "and my age is ", emp[0].age, "city is ", emp[0].city)
+// console.log("Hello I am ", emp[1].name, "and my age is ", emp[1].age, "city is ", emp[1].city)
+// console.log("Hello I am ", emp[2].name, "and my age is ", emp[2].age, "city is ", emp[2].city)
+
+// For loop
+
+// for(let agent = 0; agent < emp.length; agent++){
+//    console.log("Hello I am ", emp[agent].name, "and my age is ", emp[agent].age, "city is ", emp[agent].city)
+// }
+
+
+// let age = 12;
+
+// if(age >= 18 ){
+//   console.log("Eligible")
+// }else{
+//   console.log("Not eligible")
+// }
+
+// let color = "blue";
+
+// if(color == "red"){
+//   console.log("Yes color is red")
+// }
+// else if(color == "blue"){
+//   console.log("Yes it is blue")
+// }
+// else if(color == "orange"){
+//   console.log("Yes color is Orange")
+// }
+// else{
+//   console.log("Not found")
+// }
