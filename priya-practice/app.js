7541ea85ce38772d5604986d7c609305b9f71d04
priyasoni <sonijyoti1042@gmail.in>
2026-08-07 16:30:21 +0530

    added bootstrap product cards

---
+++ priya-practice/app.js
@@ -0,0 +1,17 @@
+alert('Welcome')
+
+let product = {
+    name: "marker CT ball Standard pen ( Black )",
+    rating: "Five star",
+    list: "add to favourite",
+    price: 220,
+}
+console.log(product)
+
+let product2 = {
+    name: "Dotted CT ball Standard pen ( Red )",
+    rating: "Five star",
+    list: "add to favourite",
+    price: 150,
+}
+console.log(typeof(product2))
