a30e0b088186c741d56d740f6f2fb5776917d1a8
priyasoni <sonijyoti1042@gmail.in>
2026-08-13 15:05:08 +0530

    updated javascript practice

--- priya-practice/app.js
+++ priya-practice/app.js
@@ -1,4 +1,8 @@
-alert('Welcome');
+// alert('Welcome');
+// document.write("hello students");
+// let result = confirm("do you want to continew");
+// let uname = prompt("enter your name");
+// alert("hello" + " " + uname);
 
 let product = {
     name: "marker CT ball Standard pen ( Black )",
@@ -14,6 +18,71 @@ let product2 = {
     list: "add to favourite",
     price: 150,
 }
-console.log(typeof(product2));
+console.log(typeof (product2));
 
 console.log("hello world");
+
+// operators //
+
+// let a = 2
+// let b = 5
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a ** b)
+
+// Unary Operators //
+
+// let a = 5
+// let b = 4
+
+// console.log("a = ", a, " & b = ", b);
+// a++;
+// console.log(a)
+
+// let a = 5
+// let b = 5
+
+// console.log("5 == 5", a == b)
+
+let a = 6
+let b = 5
+
+console.log("6 == 5", a == b)
+
+// 1. array
+
+let allcities = ["waraseoni", "balaghat", "seoni", "bhopal", "delhi", "noida", "pune"]
+// console.log(allcities[4])
+
+// 2. objects
+
+let allusers = [
+    {
+        name: "priya soni",
+        city: "waraseoni",
+        age: 22,
+        phone: 7247632099
+    }, {
+        name: "jyoti soni",
+        city: "delhi",
+        age: 23,
+        phone: 7248775469
+    }, {
+        name: "priyanka",
+        city: "jabalpur",
+        age: 25,
+        phone: 7254532099
+    }, {
+        name: "Ayushman",
+        city: "Raipur",
+        age: 24,
+        phone: 8239289389
+    }
+]
+console.log(allusers[0].name, allusers[0].city, allusers[0].age, allusers[0].phone)
+console.log(allusers[1].name, allusers[1].city, allusers[1].age, allusers[1].phone)
+console.log(allusers[2].name, allusers[2].city, allusers[2].age, allusers[2].phone)
