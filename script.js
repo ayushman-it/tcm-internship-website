da3e60faff3cc3a5e862894fb05a35ce445753d7
Ayushman <avenuedesign768@gmail.com>
2026-09-07 21:59:11 +0530

    Added

---
+++ script.js
@@ -0,0 +1,90 @@
+// alert("Hello")
+// console.log("Hello");
+// console.error("This is an error");
+
+
+// document.write("Hello");
+// confirm("Are you delete this data ?");
+// prompt("Enter Your QTY ?");
+
+
+// Var, let & const 
+
+// Keyword, name(var) = (Operator), Ayushman (Value)
+// var name = "Ayushman";
+
+
+// Decleartion
+var name;
+
+// Assignment
+name = "Ayushman";
+
+// Re assign 
+name = "Raghav";
+
+// Re assign 
+name = "Sumit";
+
+// Re Decleartion
+var name = "Amrit";
+
+// console.log(name);
+
+// -------------------------------
+
+// Dcleartion
+let fname;
+
+// Assignment
+fname = "Anirudh";
+
+// Re Assignment
+fname = "Muskan";
+
+// Re Assignment
+fname = "Sumit";
+
+// let fname = "Jagdish";
+
+
+// console.log(fname)
+
+
+// -------------------------------
+
+// const rollNumber = 293292;
+
+// let city = "Bilaspur";
+// let age = 67;
+// let accepted = false;
+// let unknown;
+// let salary = null;
+// let adhaar = BigInt(229382938293823899)
+// console.log(adhaar)
+
+
+let fruits = ["Apple", "Banana", "Grapes", "Orange"];
+// console.log(fruits[2])
+
+let obj = {
+    name: "apple",
+    price: "60/Kg"
+}
+
+let allObj = [
+    {
+        name: "apple",
+        price: "60/Kg"
+    },
+    {
+        name: "Banana",
+        price: "60/Kg"
+    },
+    {
+        name: "Grapes",
+        price: "60/Kg"
+    }
+]
+
+console.log(allObj)
