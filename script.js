b53ac22b2505ca1bff2d408a4d1b119a0a4cc43f
Ayushman <avenuedesign768@gmail.com>
2026-09-22 22:02:54 +0530

    JS DOM

--- script.js
+++ script.js
@@ -74,7 +74,7 @@
 
 // Position/Iterates/Indexing
 // -------------0-----------1----------2---------3----------4----
-let array = ["Ayushman", "Ansh", "Priyanshu", "Khushi", "Harshit"];
+// let array = ["Ayushman", "Ansh", "Priyanshu", "Khushi", "Harshit"];
 
 // console.log("Hii i am ", array[0]);
 // console.log("Hii i am ", array[1]);
@@ -202,3 +202,71 @@ let array = ["Ayushman", "Ansh", "Priyanshu", "Khushi", "Harshit"];
 //     console.log("Age is not matched..");
 // }
 
+
+
+// let array =u ["Ayshman", "Ansh", "Priyanshu", "Khushi", "Harshit"];
+
+// for(let i = 0; i < array.length; i++){
+//     console.log(i, typeof(i))
+// }
+
+// for(let i in array){
+//     console.log(i, typeof(i))
+// }
+
+// let obj = [
+//     {
+//         name: "Ayushman",
+//         age: 23,
+//         city: "bilaspur",
+//         phone: 68888787879
+//     },
+//     {
+//         name: "Vaibhav",
+//         age: 23,
+//         city: "bilaspur",
+//         phone: 68888787879
+//     }, {
+//         name: "Emon",
+//         age: 23,
+//         city: "bilaspur",
+//         phone: 68888787879
+//     }
+// ]
+
+
+// console.log(obj[1].name)
+
+// DOM - Document Object Modal
+// By Using DOM methods we can access, read, update and create html elements from javasscript.
+let heading;
+let para;
+
+// Access
+heading = document.getElementsByTagName('h1');
+para = document.getElementsByTagName('p');
+
+console.log(heading)
+console.log(para);
+
+// Read
+console.log(heading[2].innerText);
+
+// Update
+heading[0].innerText = "TCMOne";
+
+// Create
+let headingSec = document.createElement('h2');
+let body = document.getElementsByTagName('body')[0];
+
+let headingThird = document.createElement('h3');
+
+body.appendChild(headingSec);
+headingSec.innerText = "Heading made by js";
+
+body.appendChild(headingThird);
+headingThird.innerText = "Heading Third";
+
+// Decorate
+// headingSec.style.color = "red";
+headingSec.setAttribute("class", "text-red")
